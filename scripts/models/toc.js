65ff39a48f68cd03bc719a9a5d37912ce6b72512
justinHume <justin.df.hume@gmail.com>
2012-10-02 15:12:50 -0700

    Path resolution bugs for clicked links in the TOC have been fixed

--- scripts/models/toc.js
+++ scripts/models/toc.js
@@ -9,8 +9,31 @@ Readium.Models.Toc = Backbone.Model.extend({
 		this.book.on("change:toolbar_visible", this.setTocVis, this);
 	},
 
+	// Rationale: Readium expects that any hrefs to EPUB content are either absolute references or references to the content relative
+	//   to the EPUBs package document. Since any href passed to this method is specified as either absolute (in which case we 
+	//   don't need to worry) or as relative to the nav document (where the click was generated, as this is the toc), we need
+	//   to construct an absolute path from that.
 	handleLink: function(href) {
-		this.book.goToHref(href);
+
+		var TOCHref = this.book.packageDocument.getTocItem().get("href");
+
+		// If toc is in the same folder as the package document, use the href straight
+		if (TOCHref.indexOf("/") === -1) {
+
+			this.book.goToHref(href);	
+		}
+		// If the href target is in a child folder of the toc folder, create the relative URI
+		// If the href target is in a parent folder of the toc folder, this will fail, for now.
+		else {
+
+			var TOC_URI = new URI(TOCHref);
+			var targetHrefURI = new URI(href);
+
+			// Use the TOC path, relative to the package document to create an href for the target resource which will also be relative
+			//   to the package document (or absolute, if the href for the TOC was absolute).
+			href = targetHrefURI.resolve(TOC_URI).toString();
+			this.book.goToHref(href);
+		}
 	},
 
 	setVisibility: function() {
