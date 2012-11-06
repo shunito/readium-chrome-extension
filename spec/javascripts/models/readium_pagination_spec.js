d11c068fe50f369f233e32c0e18258384c43c293
justinHume <justin.df.hume@gmail.com>
2012-11-06 15:46:20 -0800

    Original pagination tests are working

--- spec/javascripts/models/readium_pagination_spec.js
+++ spec/javascripts/models/readium_pagination_spec.js
@@ -1,20 +1,5 @@
 describe("Readium.Models.ReadiumPagination", function () {
 
-    describe("defaults", function () {
-
-        beforeEach(function () {
-
-            this.epub = new Readium.Models.EPUB({"package_doc_path": "some/file/path"});
-            this.epubController = new Readium.Models.EPUBController({"epub": this.epub});
-            this.pagination = new Readium.Models.ReadiumPagination({"model": this.epubController});
-        });
-
-        it('correctly sets default attributes', function () {
-
-            expect(this.pagination.get("num_pages")).toEqual(0);
-        });
-    });
-
     describe('traversing pages', function () {
         
         beforeEach(function () {
@@ -27,7 +12,7 @@ describe("Readium.Models.ReadiumPagination", function () {
             this.pages = new Readium.Models.ReadiumPagination({"model": this.epubController});
         });
 
-        describe('reflowable section in one up', function () {
+        describe('reflowable with single page displayed', function () {
         
             beforeEach(function () {
                 
@@ -49,80 +34,79 @@ describe("Readium.Models.ReadiumPagination", function () {
 
                 spyOn(this.epubController, "getCurrentSection").andReturn(section);
             });
-        });
 
-        describe("nextPage()", function () {
-            
-            it('increments the page number if there are more pages', function () {
-            });
-
-            it('calls goToNextSection if there are no more pages', function () {
-
-                this.pages.set("num_pages", 1);
-                spyOn(this.epubController, "goToNextSection");
-                this.pages.nextPage();
-
-                expect(this.pages.get("current_page")).toEqual([1]);
-                expect(this.epubController.goToNextSection).toHaveBeenCalled();
-            });
-        });
-
-        describe("prevPage()", function () {
-
-            it('decrements the page number if there are more pages', function () {
-            });
-
-            it('calls goToPrevSection from page one', function () {
+            describe("nextPage()", function () {
                 
-                this.pages.set("current_page", [1]);
-                spyOn(this.epubController, "goToPrevSection");
-                this.pages.prevPage();
+                it('increments the page number if there are more pages', function () {
+                });
 
-                expect(this.pages.get("current_page")).toEqual([1]);
-                expect(this.epubController.goToPrevSection).toHaveBeenCalled();
+                it('calls goToNextSection if there are no more pages', function () {
+
+                    this.pages.set("num_pages", 1);
+                    spyOn(this.epubController, "goToNextSection");
+                    this.pages.nextPage();
+
+                    expect(this.pages.get("current_page")).toEqual([1]);
+                    expect(this.epubController.goToNextSection).toHaveBeenCalled();
+                });
+            });
+
+            describe("prevPage()", function () {
+
+                it('decrements the page number if there are more pages', function () {
+                });
+
+                it('calls goToPrevSection from page one', function () {
+                    
+                    this.pages.set("current_page", [1]);
+                    spyOn(this.epubController, "goToPrevSection");
+                    this.pages.prevPage();
+
+                    expect(this.pages.get("current_page")).toEqual([1]);
+                    expect(this.epubController.goToPrevSection).toHaveBeenCalled();
+                });
             });
         });
-    });
 
-    describe("reflowable section in two up", function () {
-        
-        beforeEach(function () {
-            
-            var section;
-            section = {
-                isFixedLayout: function () {
-                    return false;
-                }
-            };
+        describe('reflowable with two pages displayed', function () {
 
-            this.pages.set({
-                num_pages: 10,
-                current_page: [3, 4]
-            });
-
-            this.epubController.set({
-                two_up: true
-            });
-
-            spyOn(this.epubController, "getCurrentSection").andReturn(section);
-        });
-
-        describe("nextPage()", function () {
-            
-            it('increments both page numbers if there are more pages', function () {
-            });
-
-            it('calls goToNextSection if there are no more pages', function () {
-
-                this.pages.set("num_pages", 4);
-                spyOn(this.epubController, "goToNextSection");
-                this.pages.nextPage();
+            beforeEach(function () {
                 
-                expect(this.epubController.goToNextSection).toHaveBeenCalled();
-            });
-        });
+                var section;
+                section = {
+                    isFixedLayout: function () {
+                        return false;
+                    }
+                };
 
-        describe("prevPage()", function () {
+                this.pages.set({
+                    num_pages: 10,
+                    current_page: [3, 4]
+                });
+
+                this.epubController.set({
+                    two_up: true
+                });
+
+                spyOn(this.epubController, "getCurrentSection").andReturn(section);
+            });
+
+            describe("nextPage()", function () {
+                
+                it('increments both page numbers if there are more pages', function () {
+                });
+
+                it('calls goToNextSection if there are no more pages', function () {
+
+                    this.pages.set("num_pages", 4);
+                    spyOn(this.epubController, "goToNextSection");
+                    this.pages.nextPage();
+                    
+                    expect(this.epubController.goToNextSection).toHaveBeenCalled();
+                });
+            });
+
+            describe("prevPage()", function () {
 
             it('decrements both page numbers if there are more pages', function () {
             });
@@ -134,6 +118,7 @@ describe("Readium.Models.ReadiumPagination", function () {
                 this.pages.prevPage();
 
                 expect(this.epubController.goToPrevSection).toHaveBeenCalled();
+            });
             });
         });
     });
